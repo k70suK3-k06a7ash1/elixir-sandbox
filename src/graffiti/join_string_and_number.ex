83dd58e632ab62a95bf2b42ca2c3c29e345f2aac
2814109 <2814109k@gmail.com>
2024-10-14 10:06:28 +0900

    chore

---
+++ src/graffiti/join_string_and_number.ex
@@ -0,0 +1,21 @@
+defmodule Main do
+  def main do
+    # 整数の入力
+    {a, _} = Integer.parse(IO.gets("Enter an integer: "))
+
+    # スペース区切り整数の入力
+    input = IO.gets("Enter two integers separated by a space: ")
+    [b, c] = input
+    |> String.trim()
+    |> String.split()
+    |> Enum.map(&String.to_integer/1)
+
+    # 文字列の入力
+    s = IO.gets("Enter a string: ") |> String.trim()
+
+    # 出力
+    IO.puts("#{a + b + c} #{s}")
+  end
+end
+
+Main.main()
