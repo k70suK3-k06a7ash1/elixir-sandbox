4423b571ac19a928e73875b8f935ad1e2b5a59c1
2814109 <2814109k@gmail.com>
2024-11-02 05:17:11 +0900

    chore

--- src/competitive-programming/bmi/lib/bmi.ex
+++ src/competitive-programming/bmi/lib/bmi.ex
@@ -5,4 +5,12 @@ defmodule Bmi do
     IO.puts(denominator)
     round(weight / denominator)
   end
+
+  def rate(bmi) do
+    cond do
+      bmi < 20 -> "A"
+      bmi > 20 and bmi < 25 -> "B"
+      bmi >= 25 -> "C"
+    end
+  end
 end
--- src/competitive-programming/bmi/test/bmi_test.exs
+++ src/competitive-programming/bmi/test/bmi_test.exs
@@ -5,4 +5,16 @@ defmodule BmiTest do
   test "BMI" do
     assert Bmi.calc(170, 55) == 19
   end
+
+  test "Rate: A" do
+    assert Bmi.calc(170, 55) |> Bmi.rate() == "A"
+  end
+
+  test "Rate: B" do
+    assert Bmi.calc(160, 60) |> Bmi.rate() == "B"
+  end
+
+  test "Rate: C" do
+    assert Bmi.calc(160, 80) |> Bmi.rate() == "C"
+  end
 end
